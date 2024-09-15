7e42359830cab566769589e76658130a4b4d6f80
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-09-15 00:23:38 -0300

    Start

---
+++ back/src/main/java/back/domain/entity/Category.java
@@ -0,0 +1,20 @@
+package back.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
